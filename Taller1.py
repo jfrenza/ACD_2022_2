74def2e0a9ec197784ce560a16db3a8cd145960b
jfrenza <jfrenzac@gmail.com>
2022-10-03 15:02:57 -0500

    Puntos del taller actualizados

--- Taller1.py
+++ Taller1.py
@@ -1,1 +1,24 @@
-print('hello')
+### TALLER 1: ALGEBRA PARA CIENCIAS DE LOS DATOS
+
+## PUNTO 6
+
+# Programe la Distancia de Malahanobis utilizando la covarianza
+# habitual, luego la covarianza bajo el shrinkage de Ledoit and Wolf
+# y la covarianza y vector de medias robustas obtenida bajo el método de
+# mínima curtosis. Ilustre ejemplos concretos donde el shrinkage y el método
+# robusto presenta un mejor tendimiento y comente los resultados.
+
+## PUNTO 13
+
+# Calcule el número condición de la matriz de covarianzas de portfolio100 en su versión
+#original. Implemente alternativas para mejorarlo y reducirlo a la cuarta parte.
+
+## PUNTO 14
+
+#Sea Hn la matriz de Hilbert. Simule 1000 datos normales con matriz de covarianza
+# Hn. Estime la matriz de covarianzas desde los datos simulados. Realice una gráfica
+# de n en el eje x con el número de condición de la matriz de covarianza estimada
+# en el eje y. Qué tipo de comportamiento observa. Haga lo mismo para su determinante.
+# Realice lo mismo estimando la matriz de covarianza utilizando el shrinkage de Ledoit and Wolf.
+# Compare los resultados. Haga un análisis gráfico y de visualización donde se
+# observe si al final el shrinkage mejora el número condición.
